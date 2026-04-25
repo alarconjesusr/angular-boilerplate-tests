329a48a8b39d1c104f68f50da82f7c1727e9d460
ritchtablet <ritchtablet@gmail.com>
2026-04-24 20:14:16 -0600

    feat: Add routing for new components and implement NFT and Lifestyle sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,61 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'form',
+  },
+  {
+    path: 'form',
+    loadComponent: () => import('./form/form.component').then((m) => m.FormComponent),
+  },
+  {
+    path: 'grid',
+    loadComponent: () => import('./grid/grid.component').then((m) => m.GridComponent),
+  },
+  {
+    path: 'player',
+    loadComponent: () => import('./player/player.component').then((m) => m.PlayerComponent),
+  },
+  {
+    path: 'ngrx-signal',
+    loadComponent: () =>
+      import('./ngrx-signals/ngrx-signal.component').then((m) => m.NgrxSignalComponent),
+  },
+  {
+    path: 'http-resource',
+    loadComponent: () =>
+      import('./examples/http-resource/http-resource.component').then(
+        (m) => m.HttpResourceComponent,
+      ),
+  },
+  {
+    path: 'nft',
+    loadComponent: () => import('./landings/nft/nft-component').then((m) => m.NftComponent),
+  },
+  {
+    path: 'pinterest',
+    children: [
+      {
         path: '',
-        pathMatch: 'full',
-        redirectTo: 'form',
-    },
-	{
-		path: 'form',
-		loadComponent: () => import('./form/form.component').then(m => m.FormComponent),
-	},
-    {
-        path: 'grid',
-        loadComponent: () => import('./grid/grid.component').then(m => m.GridComponent),
-    },
-    {
-        path: 'player',
-        loadComponent: () => import('./player/player.component').then(m => m.PlayerComponent),
-    },
-    {
-        path: 'ngrx-signal',
-        loadComponent: () => import('./ngrx-signals/ngrx-signal.component').then(m => m.NgrxSignalComponent),
-    },
-    {
-        path: 'http-resource',
-        loadComponent: () => import('./examples/http-resource/http-resource.component').then(m => m.HttpResourceComponent),
-    },
+        loadComponent: () =>
+          import('./landings/pinterest-ideas/pinterest.component').then(
+            (m) => m.PinterestIdeasComponent,
+          ),
+      },
+      {
+        path: 'grids',
+        children: [
+          {
+            path: 'lifestyle',
+            loadComponent: () =>
+              import('./landings/pinterest-ideas/grids/lifestyle/lifestyle.component').then(
+                (m) => m.LifestyleComponent,
+              ),
+          },
+        ],
+      },
+    ],
+  },
 ];
---
+++ src/app/landings/nft/nft-component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-nft",
+    templateUrl: "./nft-component.html"    
+})
+export class NftComponent {
+}
